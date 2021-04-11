fff92db701920d9c158aad85c15ccfd6d68df574
EC2 Default User <ec2-user@ip-172-31-87-239.ec2.internal>
2021-04-11 15:34:12 +0000

    hoàn thành full chức năng

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 /* 
   【Todoのデータ構成】
@@ -20,27 +20,85 @@ import {getKey} from "../lib/util";
 
 function Todo() {
   const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
+      /* テストコード 開始 *  /* テストコード 終了 */
   ]);
+  useEffect(()=>{
+    let follow = JSON.parse(localStorage.getItem("follow")) || [];
+    putItems(follow)
+  },[])
 
+  const [tab,setTab]=useState("all")
+  const colorgrey={
+    color:"gray"
+  }
+  const colorblack={
+    color:"black"
+  }
+  const add=(e)=>{
+    if(e.key === "Enter"){
+      putItems([...items,{
+        key:getKey(),
+        text: e.target.value,
+        done:false
+      }])
+      console.log([...items,{
+        key:getKey(),
+        text: e.target.value,
+       }])
+       localStorage.setItem("follow", JSON.stringify([...items,{
+        key:getKey(),
+        text: e.target.value,
+        done:false
+       }]));
+    } 
+  }
+  const changecolor=(key)=>{
+    const index = items.findIndex(value=>value.key===key)
+    if (index !== -1){
+      if (items[index].done===false){
+        items[index].done=true
+      } else {
+        items[index].done=false
+      }
+      console.log(items)
+      putItems([...items])    
+    }
+  }
+  const changtab=(value)=>{
+    setTab(value)
+  }
+  const deleteTodo = ()=>{
+    putItems([])
+    localStorage.removeItem("follow")
+  }
+  let listItems=null
+  if (tab==="all"){
+    listItems=items
+  } else if (tab==="notht"){
+    listItems= items.filter(value=>value.done ===false)
+  } else {
+    listItems= items.filter(value=>value.done ===true)
+  }
+  let checked
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-      {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
+      <input class="input" type="text" onKeyDown={add}  />
+      <p onClick={()=>{changtab("all")}} >全て</p>
+      <p onClick={()=>{changtab("notht")}}>未完了</p>
+      <p onClick={()=>{changtab("ht")}}>完了済み</p>
+      {listItems.map(item => (
+        <label className="panel-block"style= { item.done===false ? colorblack: colorgrey}>
+            <input type="checkbox" onClick={()=>{changecolor(item.key)}} checked= {item.done===false ? null: "checked"} />
             {item.text}
         </label>
       ))}
       <div className="panel-block">
         {items.length} items
       </div>
+      <button onClick={deleteTodo}>全てのTODOを削除</button>
     </div>
   );
 }
